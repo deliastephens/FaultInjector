6afb30ea877546ca68249b61aa4b94d41b7f7c1c
Delia <deliasstephens@gmail.com>
2019-07-02 17:17:26 -0500

    Waypoint Converter
    Wrote script to center waypoints around new home coordinate. Added this change when processing waypoint files.

--- FaultInjector.py
+++ FaultInjector.py
@@ -7,6 +7,7 @@ import time, _thread, sys, struct, os
 from curses import ascii
 from pymavlink import mavparm, mavutil
 from pymavlink.dialects.v10 import common as mavlink
+from mission_converter import processMission
 
 # globals
 #TK panes which need to be updated
@@ -63,43 +64,28 @@ def disconnect():
   global connected
   connected = False
 
-def readMission(aFileName):
-    """
-    Load a mission from a file into a list. The mission definition is in the Waypoint file
-    format (http://qgroundcontrol.org/mavlink/waypoint_protocol#waypoint_file_format).
 
-    This function is used by upload_mission().
+def uploadMission(aFileName):
     """
+    Upload a mission from a file.
+    """
+    #Read mission from file
     global vehicle
-    global missionlist
-    global missionRead
+    missionlist = processMission(aFileName, vehicle)
 
-    print("Reading a mission")
+    print("\nUpload mission from a file: %s" % aFileName)
+    #Clear existing mission from vehicle
+    print('Clearing mission')
     cmds = vehicle.commands
-    missionlist=[]
-    with open(aFileName) as f:
-        for i, line in enumerate(f):
-            if i==0:
-                if not line.startswith('QGC WPL 110'):
-                    raise Exception('File is not supported WP version')
-            else:
-                linearray=line.split('\t')
-                ln_index=int(linearray[0])
-                ln_currentwp=int(linearray[1])
-                ln_frame=int(linearray[2])
-                ln_command=int(linearray[3])
-                ln_param1=float(linearray[4])
-                ln_param2=float(linearray[5])
-                ln_param3=float(linearray[6])
-                ln_param4=float(linearray[7])
-                ln_param5=float(linearray[8])
-                ln_param6=float(linearray[9])
-                ln_param7=float(linearray[10])
-                ln_autocontinue=int(linearray[11].strip())
-                cmd = Command( 0, 0, 0, ln_frame, ln_command, ln_currentwp, ln_autocontinue, ln_param1, ln_param2, ln_param3, ln_param4, ln_param5, ln_param6, ln_param7)
-                missionlist.append(cmd)
-    missionRead = "True"
-    return missionlist
+    print(len(cmds))
+    cmds.clear()
+    #Add new mission to vehicle
+    for command in missionlist:
+        cmds.add(command)
+    print('Uploading mission')
+    print(len(cmds))
+    vehicle.commands.upload()
+    print('Mission uploaded')
 
 
 def updateVehicleStatus(vehicle):
@@ -116,7 +102,7 @@ def updateVehicleStatus(vehicle):
         #Add battery/location/environment info
         updateText += ("\nBattery Capacity: %s MAH" % vehicle.parameters['BATT_CAPACITY'] +
                "\nGPS Info: %s" % vehicle.gps_0 +
-        "\nLattitude: %s " % vehicle.location.global_relative_frame.lat +
+        "\nLatitude: %s " % vehicle.location.global_relative_frame.lat +
         "\nLongitude: %s" % vehicle.location.global_relative_frame.lon +
            "\nAirspeed: %s" % vehicle.velocity +
            "\nAltitude: %s" % vehicle.location.global_relative_frame.alt +
@@ -205,7 +191,7 @@ def loadToolbar(root):
     fileBox.delete(0, END)
     fileBox.insert(0, "mission_basic.txt")
     fileBox.pack(side=LEFT, padx=2, pady=2)
-    fileLoad = Button(fileToolbar, text="Load", width=6, command=lambda: readMission(fileBox.get()))
+    fileLoad = Button(fileToolbar, text="Load", width=6, command=lambda: uploadMission(fileBox.get()))
     fileLoad.pack(side=LEFT, padx=2, pady=2)
 
 
